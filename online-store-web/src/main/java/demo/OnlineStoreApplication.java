ba0a770af3d6666e2e0adb8df42fea4bd17352fd
Daniel <>
2018-12-17 23:17:18 +0800

    check

--- online-store-web/src/main/java/demo/OnlineStoreApplication.java
+++ online-store-web/src/main/java/demo/OnlineStoreApplication.java
@@ -1,8 +1,22 @@
 package demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class OnlineStoreApplication extends WebSecurityConfigurerAdapter {
 
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreApplication.class, args);
+    }
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/index.html", "/login", "/", "/api/catalog/**",
+                        "/user", "/assets/**").permitAll()
+                .anyRequest().authenticated().and().csrf().disable();
+    }
 }
